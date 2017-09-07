702a848b56cdda6c61def7623b1eab483b3f67a1
shadid12 <shadidhaque2012@gmail.com>
2017-09-07 16:11:20 -0400

    router added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import Home from './components/home'
+import AddToRoom from './components/addtoroom'
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Home />
-      </div>
+      <Router>
+        <div>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/add" component={AddToRoom}/>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/addtoroom.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Navbar from './navbar/Navbar'
+
+class AddToRoom extends Component {
+	render(){
+		return(
+			<div>
+				<Navbar />
+				<span>Add now</span>
+			</div>
+		)
+	}
+}
+
+export default AddToRoom
