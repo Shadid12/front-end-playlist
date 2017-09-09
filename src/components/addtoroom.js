edfbd98ecb2db336f8163cdb48ea9536fedaa8d6
shadid12 <shadidhaque2012@gmail.com>
2017-09-09 09:45:04 -0400

    looking good with UI

--- src/components/addtoroom.js
+++ src/components/addtoroom.js
@@ -8,22 +8,42 @@ import SelectField from 'material-ui/SelectField'
 import MenuItem from 'material-ui/MenuItem'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import ReactQueryParams from 'react-query-params'
+import FlatButton from 'material-ui/FlatButton'
+import DropDownMenu from 'material-ui/DropDownMenu'
 
 class AddToRoom extends ReactQueryParams {
 	constructor(props){
 		super(props)
 		this.state = {
-			url: this.queryParams.lunch
+			url: this.queryParams.lunch,
+			value: 1
 		}
 	}
 	
+	handleChange = (event, index, value) => {
+		this.setState({value})
+	}
+
+	buttonClicked = (e) => {
+		console.log(this.state.url + ' ' + this.state.value)
+	}
 
 	render(){
 		return(
+			<MuiThemeProvider>
 			<div>
 				<Navbar />
-				<div onClick={() => {console.log(this.state.url)}}>Click</div>
+				<br />
+				<DropDownMenu value={this.state.value} onChange={this.handleChange}>
+		          <MenuItem value={1} primaryText="Never" />
+		          <MenuItem value={2} primaryText="Every Night" />
+		          <MenuItem value={3} primaryText="Weeknights" />
+		          <MenuItem value={4} primaryText="Weekends" />
+		          <MenuItem value={5} primaryText="Weekly" />
+		        </DropDownMenu>
+				<FlatButton label="Add" primary={true} onClick={this.buttonClicked} />
 			</div>
+			</MuiThemeProvider>
 		)
 	}
 }
